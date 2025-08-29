714699db47dc3a551c0c1b9e98d292a884f46d21
Owen Crook <crook.owen@gmail.com>
2025-08-29 14:28:28 -0700

    swap from full path to bucket and file

--- pkg/documents/imageupload.go
+++ pkg/documents/imageupload.go
@@ -3,9 +3,10 @@ package documents
 import "time"
 
 type ImageUploadCreate struct {
-	ID                    string    `firestore:"id" json:"id"`
-	GoogleCloudStorageUrl string    `firestore:"google_cloud_storage_url" json:"google_cloud_storage_url"`
-	LlmParsedContent      *string   `firestore:"parsed_content" json:"parsed_content,omitempty"`
-	CreatedBy             *string   `firestore:"created_by" json:"created_by,omitempty"`
-	CreatedAt             time.Time `firestore:"created_at" json:"created_at"`
+	ID               string    `firestore:"id" json:"id"`
+	Bucket           string    `firestore:"bucket" json:"bucket"`
+	Path             string    `firestore:"path" json:"path"`
+	LlmParsedContent *string   `firestore:"parsed_content" json:"parsed_content,omitempty"`
+	CreatedBy        *string   `firestore:"created_by" json:"created_by,omitempty"`
+	CreatedAt        time.Time `firestore:"created_at" json:"created_at"`
 }
